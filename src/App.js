54157ae58f7a4142548e0d30215bb81e192a4562
Antony George <antonyjoy.george@gmail.com>
2023-04-11 22:00:08 +0530

    closes #8; Add SearchBox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -35,13 +36,11 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="Search Monsters"
-          onChange={onSearchChange}
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder={"Search Monsters"}
+          className={"search-box"}
         />
-
         <CardList monsters={filteredMonsters} />
       </div>
     );
